2fb5d904a593bb6ac42a082b9f6daff642cee8ce
Anas Abubakar <anasabubakar7000@gmail.com>
2026-01-07 15:14:56 +0100

    Content update: condensed About section text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import './About.css'
 import ScrollReveal from './ScrollReveal'
 
 const About = () => {
-    const text1 = "PyData is an educational program of NumFOCUS, a 501(c)(3) nonprofit organization in the United States. PyData provides a forum for the international community of users and developers of data analysis tools to share ideas and learn from each other. The global PyData network promotes discussion of best practices, new approaches, and emerging technologies for data management, processing, analytics, and visualization. PyData communities approach data science using many languages, including (but not limited to) Python, Julia, and R."
+    const text1 = "PyData is an educational program of NumFOCUS, a 501(c)(3) nonprofit organization in the United States. PyData provides a forum for the international community of users and developers of data analysis tools to share ideas and learn from each other. The global PyData network promotes discussion of best practices, new approaches, and emerging technologies for data management, processing, analytics, and visualization."
     
-    const text2 = "PyData Lagos brings together data enthusiasts, researchers, and professionals across various domains to exchange knowledge, share experiences, and explore innovative techniques. Our community is especially passionate about using data science and analytics to address Africa’s unique challenges, while contributing to the global open-source ecosystem."
+    const text2 = "PyData Lagos brings together data enthusiasts, researchers, and professionals to exchange knowledge and explore innovative techniques. Our community is passionate about using data science to address Africa’s unique challenges, while contributing to the global open-source ecosystem."
 
     return (
         <section className="about">
